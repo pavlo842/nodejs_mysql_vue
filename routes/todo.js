206a97b40c13d5d06f6b1a54af62e91f216f6659
Pavlo842 <pavlo842@mail.ru>
2020-01-02 23:45:59 +0300

    9-3 api

---
+++ routes/todo.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+const router = Router()
+
+// Получение списка задач
+router.get('/', (req, res) => {
+    // res.json({a: 1}) // проверка работоспособности метода get
+})
+
+// Создание задачи
+router.post('/', (req, res) => {})
+
+// Изменение задачи
+router.put('/:id', (req, res) => {})
+
+// Удаление задачи
+router.delete('/:id', (req, res) => {})
+
+
+module.exports = router
