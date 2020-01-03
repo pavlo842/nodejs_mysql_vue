08798e2ed01709ae53732f9198c6d563b3130bb9
Pavlo842 <pavlo842@mail.ru>
2020-01-04 00:26:46 +0300

    9-5 add db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,4 +13,14 @@ app.use((req, res, next) => {
     res.sendFile('/index.html')
 })
 
-app.listen(PORT)
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(PORT)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
